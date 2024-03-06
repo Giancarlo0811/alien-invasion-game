ce2b7db87c76b19c6ccd6f5c63130fd4071adab9
Giancarlo <giancarlo.t0811@gmail.com>
2024-03-05 21:00:30 -0400

    movimiento de nave implementado

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,11 +16,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Crear nave
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Iniciar loop principal del juego
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
+
         gf.update_screen(ai_settings, screen, ship)
 
 
--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame.image
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Inicializar nave y establecer su posicion inicial"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Cargar la imagen de la nave
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,21 @@ class Ship():
         # Iniciar cada nave nueva abajo en el centro
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Guardar valor para la nave en el centro
+        self.center = float(self.rect.centerx)
+
+        # Estado del movimiento
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Actualizar movimiento de la nave"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Dibujar la nave en su posicion actual"""
